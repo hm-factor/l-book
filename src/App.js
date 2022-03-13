42902fa671830f5d46544b37abcf44b683ccaddc
hm-factor <hsmanel@gmail.com>
2022-03-13 14:08:00 -0400

    add cover

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import cover from './book_cover.jpeg';
 
 function App() {
   return (
     <div className="App">
       <div>hi</div>
+      <img src={cover} alt="cover" />
     </div>
   );
 }
