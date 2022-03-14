aa2a38ea0bc17e2dfa7d1d88716b21fe77975823
hm-factor <hsmanel@gmail.com>
2022-03-13 23:30:45 -0400

    general layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import cover from './book_cover.jpeg';
+import "./App.css";
+import cover from "./book_cover.jpeg";
 
 function App() {
   return (
-    <div className="App">
-      <div>hi</div>
-      <img src={cover} alt="cover" />
-    </div>
+	<div className="App">
+		<div className="content">
+			<div className="navbar">
+				<div className="navbar-item">Purchase</div>
+				<div className="navbar-item">Contact</div>
+			</div>
+			<div className="cover-container">
+				<img src={cover} alt="cover" className="cover-img"/>
+				<div className="author">
+					by Eleanor Amicucci
+				</div>
+			</div>
+		</div>
+	</div>
   );
 }
 
