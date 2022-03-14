771dd0b0a92f632c17ed2b9776614443c4202cb4
hm-factor <hsmanel@gmail.com>
2022-03-13 23:32:08 -0400

    remove author div

--- src/App.js
+++ src/App.js
@@ -11,9 +11,6 @@ function App() {
 			</div>
 			<div className="cover-container">
 				<img src={cover} alt="cover" className="cover-img"/>
-				<div className="author">
-					by Eleanor Amicucci
-				</div>
 			</div>
 		</div>
 	</div>
