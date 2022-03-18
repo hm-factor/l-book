cde64f79ba7bd3732dcb4f4d2cc9a2e2551b326b
hm-factor <hsmanel@gmail.com>
2022-03-18 19:30:10 -0400

    links popup content styled

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -6,26 +6,28 @@ export default function Popup(props) {
     }
 
     let closeBtn = (
-      <div>
-        <button onClick={closePopup}>close</button>
-      </div>
+        <div >
+            <button className="close-btn" onClick={closePopup}>close</button>
+        </div>
     );
 
     return (
         <div className={`popup-container ${popup ? "" : "off"}`}>
             {popup === "purchase" && (
-                <div className="popup-content ">
+                <div className="popup-content">
                     <div className="links">
-                        <a href="google.com">google</a>
-                        <a href="amazon.com">amazon</a>
-                        <a href="abebooks.com">abe</a>
+                        <a target="_blank" rel="noopener" href="google.com">google</a>
+                        <a target="_blank" rel="noopener" href="amazon.com">amazon</a>
+                        <a target="_blank" rel="noopener" href="abebooks.com">abe</a>
                     </div>
                     {closeBtn}
                 </div>
             )}
             {popup === "contact" && (
-                <div>
-                    contact
+                <div className="popup-content">
+                    <div className="">
+
+                    </div>
                     {closeBtn}
                 </div>
             )}
