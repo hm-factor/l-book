84cbc8792e497fd91314976619063a004d786d3f
hm-factor <hsmanel@gmail.com>
2022-03-18 18:05:19 -0400

    start creating some popup containers and re styled the container

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import "./App.css";
 import cover from "./book_cover.jpeg";
 
-function App() {
-  return (
-	<div className="App">
-		<div className="content">
-			<div className="navbar">
-				<div className="navbar-item">Purchase</div>
-				<div className="navbar-item">Contact</div>
-			</div>
-			<div className="cover-container">
-				<img src={cover} alt="cover" className="cover-img"/>
+function LinkPopup() {
+	return (
+		<div className="popup-container">
+			<div className="popup-content links">
+				<a href="">google</a>
+				<a href="">amazon</a>
+				<a href="">abe</a>
 			</div>
 		</div>
-	</div>
-  );
+	)
+}
+
+function ContactPopup() {
+	return (
+		<div className="popup-container">
+			
+		</div>
+	)
+}
+
+function App() {
+
+
+	return (
+		<div className="App">
+			<div className="content">
+				<div className="navbar">
+					<div className="navbar-item">Purchase</div>
+					<div className="navbar-item">Contact</div>
+				</div>
+				<div className="cover-container">
+					<img src={cover} alt="cover" className="cover-img"/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
