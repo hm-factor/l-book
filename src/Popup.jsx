c63212b3e910b167056972b239aebd82368910d7
hm-factor <hsmanel@gmail.com>
2022-03-28 19:07:05 -0400

    add actual links to popup

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -12,35 +12,46 @@ export default function Popup(props) {
     );
 
     return (
-        <div className={`popup-container ${popup ? "" : "off"}`}>
-            {popup === "purchase" && (
-                <div className="popup-content">
-                    <div className="links">
-                        <a target="_blank" rel="noopener" href="google.com">this is a link</a>
-                        <a target="_blank" rel="noopener" href="amazon.com">also a link</a>
-                        <a target="_blank" rel="noopener" href="abebooks.com">not a link</a>
-                    </div>
-                    {closeBtn}
-                </div>
-            )}
-            {popup === "contact" && (
-                <div className="popup-content contact-content">
-                    <form className="contact-form" action="submit">
-                        <label className="contact-section">
-                            name/email
-                            <input type="text" />
-                        </label>
-                        <label className="contact-section">
-                            message
-                            <input className="contact-body" type="text" />
-                        </label>
-                        <button type="submit" className="send-btn">
-                            Send!
-                        </button>
-                    </form>
-                    {closeBtn}
-                </div>
-            )}
-        </div>
+      <div className={`popup-container ${popup ? "" : "off"}`}>
+        {popup === "purchase" && (
+          <div className="popup-content">
+            <div className="links">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.barnesandnoble.com/w/soon-ill-be-from-the-soil-someday-eleanor-amicucci/1141262741?ean=9780578390802"
+              >
+                Barnes & Noble
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.amazon.com/dp/0578390809?tag=bookfinder-test-b2-20&linkCode=osi&th=1&psc=1&language=en_US&selectObb=new"
+              >
+                Amazon
+              </a>
+            </div>
+            {closeBtn}
+          </div>
+        )}
+        {popup === "contact" && (
+          <div className="popup-content contact-content">
+            <form className="contact-form" action="submit">
+              <label className="contact-section">
+                name/email
+                <input type="text" />
+              </label>
+              <label className="contact-section">
+                message
+                <input className="contact-body" type="text" />
+              </label>
+              <button type="submit" className="send-btn">
+                Send!
+              </button>
+            </form>
+            {closeBtn}
+          </div>
+        )}
+      </div>
     );
 }
